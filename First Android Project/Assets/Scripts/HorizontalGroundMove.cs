91097f430344adc9eb7869d34f4e884d6702c837
ZqhKHP <G00336837@gmit.ie>
2018-10-08 22:15:22 +0100

    adding MovingPlatform script

---
+++ First Android Project/Assets/Scripts/HorizontalGroundMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalGroundMove : MonoBehaviour {
+    public int groundSpeed;
+    public int xMove;
+    public float flipDistance = 0.7f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+         RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xMove, 0));
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMove, 0) * groundSpeed;
+        // RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xMove, 0));
+        //gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMove, 0) * groundSpeed;
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xMove,0));
+       
+
+        if (hit.distance < flipDistance)
+        {
+            flip();
+            /*
+            if (hit.collider.tag == "Player")
+            {
+                Destroy(hit.collider.gameObject);
+                levelManager.ChangeLevel("LevelsMenu");
+            }
+            */
+        }
+
+    }
+    void flip()
+    {
+        if (xMove > 0)
+        {
+            xMove = -1;
+        }
+        else
+        {
+            xMove = 1;
+        }
+    }
+}
---
+++ First Android Project/Assets/Scripts/MovinPlatform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovinPlatform : MonoBehaviour {
+
+    private Vector3 posA;
+
+    private Vector3 posB;
+
+    private Vector3 nextPosition;
+
+    public float speed;
+
+    public Transform childTransform;
+
+    public Transform transformB;
+
+	// Use this for initialization
+	void Start () {
+
+        posA = childTransform.localPosition;
+        posB = transformB.localPosition;
+        nextPosition = posB;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nextPosition, speed * Time.deltaTime);
+
+        if (Vector3.Distance(childTransform.localPosition, nextPosition) <= 0.1)
+        {
+            ChangeDirection();
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        nextPosition = nextPosition != posA ? posA : posB;
+    }
+}
