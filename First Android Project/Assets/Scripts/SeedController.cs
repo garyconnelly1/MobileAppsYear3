75dec2167882594d317aeaa14ebc7c1317030a8e
ZqhKHP <G00336837@gmit.ie>
2018-10-16 14:02:09 +0100

    adding seed controller script

---
+++ First Android Project/Assets/Scripts/SeedController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedController : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime,
+           transform.position.y); // move the seed accross the screen on the x axis
+
+    }
+}
--- First Android Project/Assets/Scripts/ShootingScript.cs
+++ First Android Project/Assets/Scripts/ShootingScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ShootingScript : MonoBehaviour {
-
+    /*
     public float damage = 10f;
 
     Touch touch;
@@ -32,24 +32,47 @@ public class ShootingScript : MonoBehaviour {
             Shoot();
         }
 
-        if (Input.GetMouseButtonDown)
-        {
-            Shoot();
-        }
+       
     }
 
     void Shoot()
     {
         // Debug.Log("touch position " + touch);
-        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
-            Camera.main.ScreenToWorldPoint(Input.mousePosition).y); // get position of touch point
+        /*  Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+              Camera.main.ScreenToWorldPoint(Input.mousePosition).y); // get position of touch point
 
-        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y); // get the fire point
+          Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y); // get the fire point
 
-        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100);
+          RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100);
 
-        Debug.DrawLine(firePointPosition, mousePosition);
+          Debug.DrawLine(firePointPosition, mousePosition);  
+
+
+
+
+    }*/
+
+
+
+
+    public GameObject bullet;
+    public Transform spawnPoint;
+
+    void Update()
+    {
+        // touch = Input.GetTouch(0);
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Shoot();
+        }
 
 
     }
-}
+
+    void Shoot()
+    {
+        Debug.Log("shoot");
+        Instantiate(bullet, spawnPoint.position, Quaternion.identity); // create the obstacle object
+    }
+
+    }
