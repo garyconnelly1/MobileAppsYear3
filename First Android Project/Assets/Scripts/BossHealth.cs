494707ba6c696ad6565c66d3755e3d5cfac47d99
ZqhKHP <G00336837@gmit.ie>
2018-10-20 19:08:07 +0100

    lose conditions for the boss

--- First Android Project/Assets/Scripts/BossHealth.cs
+++ First Android Project/Assets/Scripts/BossHealth.cs
@@ -3,23 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BossHealth : MonoBehaviour {
+    public int bossScore;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        bossScore = 50;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        if (bossScore == 0)
+        {
+            //Debug.Log("Kill boss");
+            Die();
+        }
 		
 	}
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("Inside boss health");
+        
         if (other.gameObject.tag == "Seed") // if the boss hits the player
         {
             Destroy(other.gameObject); // destroy that seed object
+            bossScore -= 10;
         }
+    }
+
+    void Die()
+    {
+        Debug.Log("Kill boss");
+        Destroy(this.gameObject);
     }
 }
