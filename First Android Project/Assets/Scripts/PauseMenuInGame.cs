9d0387485c7230f20d50b708cbc8eba7005e480d
ZqhKHP <G00336837@gmit.ie>
2018-11-19 16:35:27 +0000

    adding comments and referencing to the pause menu script

--- First Android Project/Assets/Scripts/PauseMenuInGame.cs
+++ First Android Project/Assets/Scripts/PauseMenuInGame.cs
@@ -11,45 +11,45 @@ public class PauseMenuInGame : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape)) // if the user presses the escape key while using a keyboard.
         {
-            PauseGame();
+            PauseGame(); // trigger the pause game method.
         }
 
     }
-
-    public void PauseGame()
+    /* Adapted from: https://www.youtube.com/watch?v=JivuXdrIHK0 */
+    public void PauseGame() // method is triggered when the user presses the escape key or the pause UI button.
     {
-            if (IsPaused)
+            if (IsPaused) // If the game is already paused.
             {
-                Resume();
+                Resume(); // resume the game.
             }
-            else
+            else // if the game is not currently paused.
             {
-                PauseMenu();
+                PauseMenu(); // activate the pause menu.
             }
     }
 
     public void Resume()
     {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        IsPaused = false;
+        pauseMenuUI.SetActive(false); // dea-activate the pause menu.
+        Time.timeScale = 1f; // return the time scale to normal(1 second at a time).
+        IsPaused = false; // set the IsPaused variable back to false.
     }
 
-    public void PauseMenu()
+    public void PauseMenu() // whenever the pause menu is activated.
     {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        IsPaused = true;
+        pauseMenuUI.SetActive(true); // set the visibility of the panel to true.
+        Time.timeScale = 0f; // set the time scale to 0 so all the animations and timers appear to pause.
+        IsPaused = true; // set the isPaused variable to true while this menu is displayed.
     }
 
     public void MainMenu()
     {
-        IsPaused = false;
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        manager.ChangeLevel(Levels.Main_menu);
+        IsPaused = false; // set the IsPaused variable to false so that is the user goes to main menu then exits the game, the level will start as normal next time around.
+        pauseMenuUI.SetActive(false); // de-activate the pause menu.
+        Time.timeScale = 1f; // return the time scale to normal(1 second at a time).
+        manager.ChangeLevel(Levels.Main_menu); // use the level manager to change the view to the main menu.
     }
 
     public void Volume()
