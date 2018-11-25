9c525434518623e1296e0ce72bc02183a0fd4a82
ZqhKHP <G00336837@gmit.ie>
2018-11-25 13:51:18 +0000

    adding a main theme sound for each level

---
+++ First Android Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+/* Adapted from https://www.youtube.com/watch?v=6OT43pvUyfY&t=693s */
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds; // so that we can manage all of our audio files from this class
+
+	// Use this for initialization
+	void Awake () {
+        foreach (Sound s in sounds) // loop through each audio source and give it the following properties
+        {
+            s.source = gameObject.AddComponent<AudioSource>(); // add Audio source component
+
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+	}
+
+    void Start()
+    {
+        Play("MainTheme"); // anytime the audion manager is in a level, automatically play the main theme at the start of the level
+    }
+	
+	public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name); // call back function for finding the audio source in the array
+        s.source.Play(); // play the audio
+    }
+}
---
+++ First Android Project/Assets/Scripts/Sound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound  { // base class for sound objects
+
+    public string name; // each sound needs a name
+
+    public AudioClip clip; // each sound has an audio clip
+
+    [Range(0f, 1f)]
+    public float volume; // give each sound a range for column
+
+    [Range(0.1f,3)]
+    public float pitch; // give each sound a pitch
+
+    [HideInInspector]
+    public AudioSource source; // give each sound an audio source
+
+    public bool loop; // set whether or not the sound will loop
+}
