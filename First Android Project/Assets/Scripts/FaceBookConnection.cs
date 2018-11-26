6817361aa5c76120389f78e84dccb5227119d1e5
ZqhKHP <G00336837@gmit.ie>
2018-11-26 13:20:45 +0000

    Facebook login and log out done

--- First Android Project/Assets/Scripts/FaceBookConnection.cs
+++ First Android Project/Assets/Scripts/FaceBookConnection.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
 
 public class FaceBookConnection : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	private void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized)
+                {
+                    FB.ActivateApp();
+                }
+                else
+                {
+                    Debug.Log("Couldn't Initialize Facebook");
+                }
+            },
+            isGameShown =>
+            {
+                if (isGameShown)
+                    Time.timeScale = 0;
+                else
+                    Time.timeScale = 1;
+
+            });
+        }
+        else
+            FB.ActivateApp();
+    }
+
+    public void FaceBookLogin()
+    {
+        
+        var permissions = new List<string>() { "public_profile", "email", "user_friends" };
+        FB.LogInWithReadPermissions(permissions);
+    }
+
+    public void FaceBookLogOut()
+    {
+        FB.LogOut();
+    }
+
+    public void Share()
+    {
+       // FB.ShareLink(new System.Uri("https://github.com/garyconnelly1/MobileAppsYear3", "High Score!", "game", ));
+    }
 }
